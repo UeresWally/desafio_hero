b014d535f4e97141d05c51af5a3e5b73877ea2e2
Wallace Caetano <wallace.santos@youtan.com.br>
2024-08-10 20:07:18 -0300

    update:  atualiza endpoint para receber cidade e buscar uma playlist de acordo com a temperatura

--- app/api/v1/endpoints/weather.py
+++ app/api/v1/endpoints/weather.py
@@ -1,13 +1,13 @@
 from fastapi import APIRouter, HTTPException
-
-
+from app.services.music_service import get_playlist
+from app.services.weather_service import get_temperature
 router = APIRouter()
 
 @router.get("/playlist/")
 async def get_playlist_by_city(city: str):
     try:
-        temperature = 15.0
-        playlist = ['Faint', 'Lonely day', 'Sultans of swing', 'Foxy Lady']
+        temperature = await get_temperature(city)
+        playlist = get_playlist(temperature)
         return {"city": city, "temperature": temperature, "playlist": playlist}
     except Exception as e:
         raise HTTPException(status_code=400, detail=str(e))
