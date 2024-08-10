2ef564d3064bfa9815af82d5f537e1ef97b68c0a
Wallace Caetano <wallace.santos@youtan.com.br>
2024-08-10 20:06:05 -0300

    feat: adiciona weather service que pega temperatura atual de uma cidade

--- app/core/config.py
+++ app/core/config.py
@@ -1,8 +1,12 @@
 from pydantic_settings import BaseSettings
+from dotenv import load_dotenv
+
+load_dotenv()
 
 class Settings(BaseSettings):
     SPOTIFY_CLIENT_ID: str
     SPOTIFY_CLIENT_SECRET: str
+    WEATHER_API_KEY: str
 
     class ConfigDict:
         env_file = ".env"
